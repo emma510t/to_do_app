7b686ad19f32c81990ecdc8380293ec3304a8d85
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-09-11 14:06:55 +0200

    yay noget virker

--- js/main.js
+++ js/main.js
@@ -2,21 +2,52 @@
 
 const btn = document.querySelector("button");
 const toDoList = document.querySelector("#to_do :nth-child(2)");
-const task = {
-  id: 0,
-  desc: "task_text",
-  date: "date_text",
-  fav: false,
-};
 const taskArray = [];
+let taskID = 1;
 
 btn.addEventListener("click", addTask);
 
 function addTask(evt) {
-  evt.preventDefault();
-  let inputText = document.getElementById("input_text").value;
-  console.log(inputText);
+  if (document.querySelector("#input_text").value !== "") {
+    evt.preventDefault();
+    const inputText = document.getElementById("input_text");
+    const inputDate = document.getElementById("input_date");
+    console.log(inputText);
 
-  task.desc = inputText;
-  console.log(taskArray);
+    const task = {
+      id: taskID++,
+      desc: inputText.value,
+      date: inputDate.value,
+      fav: false,
+    };
+
+    taskArray.push(task);
+
+    console.log(taskArray);
+
+    /// clear input
+
+    inputText.value = "";
+    inputDate.value = "";
+
+    displayTask(task);
+  }
 }
+
+function displayTask(task) {
+  const clone = document.querySelector("template").content.cloneNode(true);
+  clone.querySelector("[data-field=desc]").textContent = task.desc;
+  clone.querySelector("[data-field=date]").textContent = task.date;
+  //clone.querySelector("[data-field=fav]");
+  toDoList.appendChild(clone);
+}
+
+/* function displayTask(array) {
+  array.forEach((task) => {
+    const clone = document.querySelector("template").content.cloneNode(true);
+    clone.querySelector("[data-field=desc]").textContent = task.desc;
+    clone.querySelector("[data-field=date]").textContent = task.date;
+    //clone.querySelector("[data-field=fav]");
+    toDoList.appendChild(clone);
+  });
+} */
