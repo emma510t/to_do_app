f36831e97a6ae7e1021230b690037f90d586548d
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-09-11 14:59:42 +0200

    local storage :)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 "use strict";
-
+window.addEventListener("load", displayLocalTask);
 const btn = document.querySelector("button");
 const toDoList = document.querySelector("#to_do :nth-child(2)");
-const taskArray = [];
-let taskID = 1;
+let taskArray = [];
 
+function displayLocalTask() {
+  taskArray = JSON.parse(localStorage.getItem("array"));
+  if (taskArray !== null) {
+    console.log(taskArray);
+    taskArray.forEach((task) => {
+      const clone = document.querySelector("template").content.cloneNode(true);
+      clone.querySelector("[data-field=desc]").textContent = task.desc;
+      clone.querySelector("[data-field=date]").textContent = task.date;
+      //clone.querySelector("[data-field=fav]");
+      toDoList.appendChild(clone);
+    });
+  }
+}
 btn.addEventListener("click", addTask);
 
 function addTask(evt) {
@@ -14,14 +26,24 @@ function addTask(evt) {
     const inputDate = document.getElementById("input_date");
     console.log(inputText);
 
+    let idValue = 1;
+    if (taskArray === null) {
+      taskArray = [];
+    }
+    if (taskArray.length > 0) {
+      idValue = taskArray[taskArray.length - 1].id + 1;
+    }
+
     const task = {
-      id: taskID++,
+      id: idValue,
       desc: inputText.value,
       date: inputDate.value,
       fav: false,
     };
 
     taskArray.push(task);
+
+    localStorage.setItem("array", JSON.stringify(taskArray));
 
     console.log(taskArray);
 
@@ -41,13 +63,3 @@ function displayTask(task) {
   //clone.querySelector("[data-field=fav]");
   toDoList.appendChild(clone);
 }
-
-/* function displayTask(array) {
-  array.forEach((task) => {
-    const clone = document.querySelector("template").content.cloneNode(true);
-    clone.querySelector("[data-field=desc]").textContent = task.desc;
-    clone.querySelector("[data-field=date]").textContent = task.date;
-    //clone.querySelector("[data-field=fav]");
-    toDoList.appendChild(clone);
-  });
-} */
